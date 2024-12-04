32e1ee1eb541dcbe93f182866e7f885afd9e2bde
oso082024 <oso082024@gmail.com>
2024-12-04 09:57:58 +0100

    divided and sorted numbers in arrays

--- Dates/1st.cs
+++ Dates/1st.cs
@@ -5,8 +5,28 @@ public class Day1
     public int TotalDistance(string str)
     {
         int result = 0;
-        string[] pairs = str.Replace("   ", "_").Split(" ");
-        foreach (string s in pairs) Console.WriteLine(s);
+        string[] pairs = str.Replace("   ", " ").Split(" ");
+        int[] numbersLeft = new int[pairs.Length / 2];
+        int[] numbersRight = new int[pairs.Length /2];
+        int l = 0; int r = 0;
+        for(int i = 0; i < pairs.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                numbersLeft[l] = Convert.ToInt32(pairs[i]); 
+                l++;
+            }
+            else
+            {
+                numbersRight[r] = Convert.ToInt32(pairs[i]);
+                r++; 
+            }
+
+        }
+        Array.Sort(numbersLeft);
+        Array.Sort(numbersRight);
+        foreach (int i in numbersLeft) Console.WriteLine(i);
+
         return result;
     }
 }
