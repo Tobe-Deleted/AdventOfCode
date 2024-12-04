9c2ab149441a772452ed5d9a0bcbf45d63c6bd2e
oso082024 <oso082024@gmail.com>
2024-12-04 12:01:52 +0100

    Translated each report into int arrays

--- Dates/2nd.cs
+++ Dates/2nd.cs
@@ -8,7 +8,13 @@ public class Day2
         var reports = File.ReadLines(filePath);
         foreach(string report in reports)
         {
-            Console.WriteLine(report);
+            string[] strArr = report.Split(' ');
+            int[] currentReport = new int[strArr.Length];
+            for(int i = 0; i < strArr.Length; i++)
+            {
+                currentReport[i] = Convert.ToInt32(strArr[i]);
+            }
+            
         }
         return safeReports;
     }
