087cd95c300c28f2666dbd619435346372555b52
oso082024 <oso082024@gmail.com>
2024-12-06 15:42:32 +0100

    Day6 part 2 coordinates list added

--- Days/6th.cs
+++ Days/6th.cs
@@ -7,22 +7,26 @@ public class Day6
         int dx = -1; int dy = 0;
         int x = 59; int y = 62;
         var map = File.ReadAllLines(filePath);
-        string[,] markedMap = new string[130,130];
-        string path = "";
+        // string[,] markedMap = new string[130,130];
+        // string path = "";
+        List<int[]> coordinates = new List<int[]>{};
         
         while(true)
         {
             if(x+dx < 0 || x+dx > 129 ||
                y+dy < 0 || y+dy > 129) // end logic
             {
-                markedMap[x,y] = 'X'.ToString();
+                int[] temp = {x, y};
+                coordinates.Add(temp);
+                // markedMap[x,y] = 'X'.ToString();
 
-                foreach(string line in markedMap)
-                {
-                    path += line;
-                }
+                // foreach(string line in markedMap)
+                // {
+                //     path += line;
+                // }
 
-                return path.Count(c => c == 'X');
+                //return path.Count(c => c == 'X');
+                break;
             }
 
             if(map[x+dx][y+dy] == '#') //change direction
@@ -31,9 +35,16 @@ public class Day6
             }
             else                        //straight path
             {
-                markedMap[x,y] = 'X'.ToString();
+                // markedMap[x,y] = 'X'.ToString();
                 y += dy; x += dx;
+                int[] temp = {x,y};
+                coordinates.Add(temp);
             }
+        }
+
+        while(true)
+        {
+            
         }
     }
 }
