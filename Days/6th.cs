cfc0be2ba057c12e644a45dec117ef1e0501b8b3
oso082024 <oso082024@gmail.com>
2024-12-06 15:32:46 +0100

    Day6 complete

--- Days/6th.cs
+++ Days/6th.cs
@@ -1,11 +1,39 @@
+using System.Data.Common;
+
 public class Day6
 {
     public int GuardPath(string filePath)
     {
-        int x = 0; int y = -1;
-        var map = File.ReadAllLines()
-        //string[] map = new string[130]; // 0-129
+        int dx = -1; int dy = 0;
+        int x = 59; int y = 62;
+        var map = File.ReadAllLines(filePath);
+        string[,] markedMap = new string[130,130];
+        string path = "";
         
-        return 0;
+        while(true)
+        {
+            if(x+dx < 0 || x+dx > 129 ||
+               y+dy < 0 || y+dy > 129) // end logic
+            {
+                markedMap[x,y] = 'X'.ToString();
+
+                foreach(string line in markedMap)
+                {
+                    path += line;
+                }
+
+                return path.Count(c => c == 'X');
+            }
+
+            if(map[x+dx][y+dy] == '#') //change direction
+            {
+                (dx, dy) = (dy, -dx);
+            }
+            else                        //straight path
+            {
+                markedMap[x,y] = 'X'.ToString();
+                y += dy; x += dx;
+            }
+        }
     }
 }
--- obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b8bde1f49fb1f4447a9d99dff7091ffb58ba71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b04bcbf2ffa8b7061d83b751539d5e389d76d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
