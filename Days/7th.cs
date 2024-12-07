fc5f0df4bc8f074fb249d9f64c871c22e97eead4
oso082024 <oso082024@gmail.com>
2024-12-07 19:25:42 +0100

    Day 7 logic for strings to ints set up

--- Days/7th.cs
+++ Days/7th.cs
@@ -4,7 +4,18 @@ public class Day7
     {
         int finalSum = 0;
         string[] input = File.ReadAllLines(filePath);
-        
+        foreach (string equationString in input)
+        {
+            int Sum = Convert.ToInt32(equationString.Substring(0, equationString.IndexOf(':')));
+            string[] temp = equationString.Remove(0, equationString.IndexOf(' '))
+                                          .Trim()
+                                          .Split(' ');
+            int[] numbers = new int[temp.Length];
+            for(int i = 0; 0 < temp.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(temp[i]);
+            }
+        }
         return finalSum;
     }
 }
