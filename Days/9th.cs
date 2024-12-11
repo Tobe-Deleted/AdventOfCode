cda20e04b396c14f1551ac0cd1af401660957e02
oso082024 <oso082024@gmail.com>
2024-12-11 07:46:53 +0100

    Day9 part 1, found a new possible solution

--- Days/9th.cs
+++ Days/9th.cs
@@ -1,73 +1,65 @@
+using System.Data.Common;
+using System.Globalization;
+
 public class Day9()
 {
     public long DiskFragmenter(string filePath)
     {
         long result = 0;
         string rawData = File.ReadAllText(filePath);
-        char[] filePointers = new char[rawData.Length /2 +1];
-        char[] emptyPointers = new char[rawData.Length /2];
+        int[] filePointers = new int[rawData.Length];
         int fp = 0;
-        int ep = 0;
 
         for (int i = 0; i < rawData.Length; i++)
         {
-            if(i % 2 == 0)
-            {
-                // Console.WriteLine($"{fp}, {filePointers.Length}");
-                filePointers[fp] = rawData[i];
+                filePointers[fp] = Convert.ToInt32(rawData[i] -48);
                 fp++;
-            }
-            else
-            {
-                // Console.WriteLine(rawData[i]);
-                emptyPointers[ep] = rawData[i];
-                ep++;
-            }
         }
-        // foreach(char ch in emptyPointers)Console.WriteLine(ch);
-        string blockExpander = "";
-        int[] IDtracker = new int[filePointers.Length];
+
+        List<int> blockExpander= new List<int>{};
 
         for(int i = 0; i < filePointers.Length; i++)
         {
-            Console.WriteLine(i);
-            for(int n = 0; n < Convert.ToInt32(filePointers[i])-48; n++)
+            // Console.WriteLine($"{i}, {filePointers[i]}");
+            if(i % 2 == 0)
             {
-                blockExpander += $"{i}";
-                IDtracker[i] = $"{i}".Length;
-            }
-            // Console.WriteLine($"{i}, {emptyPointers.Length}");
-            if(i != emptyPointers.Length)
-            {
-                // Console.WriteLine($"{Convert.ToInt32(emptyPointers[i]-48)}");
-                for(int j = 0; j < Convert.ToInt32(emptyPointers[i] -48); j++)
+                for(int j = 0; j <= filePointers[i]; j++)
                 {
-                    blockExpander += ".";
+                    blockExpander.Add(i);
+                }
+            }
+            else
+            {
+                for(int j = 0; j <= filePointers[i]; j++)
+                {
+                    blockExpander.Add(-1);
+                }
+            }
+
+        }
+                    foreach(int i in blockExpander)Console.WriteLine(i);
+
+        
+        for(int i = 0; i < blockExpander.Count(); i++)
+        {
+            if(blockExpander[blockExpander.Count()-1 -i] != -1)
+            {
+                for(int j = 0; i < blockExpander.Count(); j++)
+                {
+                    if(blockExpander[j] == -1)
+                    {
+                        blockExpander[j], blockExpander[blockExpander.Count()-1 -i]
+                    }
                 }
             }
         }
-                    Console.WriteLine(blockExpander);
 
-        string dataCompacter = blockExpander;
 
-        for(int i = 0; i < blockExpander.Count(c => c == '.'); i++)
-        {
-            for(int j = 1; j < IDtracker[IDtracker.Length -i]; j++)
-            {
-                Console.WriteLine(j);
-            dataCompacter = dataCompacter.Insert(dataCompacter.IndexOf('.'), dataCompacter[dataCompacter.Length -1].ToString())
-                                         .Remove(dataCompacter.Length -1, 1);
-            }
-            dataCompacter = dataCompacter.Remove(dataCompacter.IndexOf('.'), 1);
-        }
-
-        Console.WriteLine(dataCompacter);
-
-        for(int i = 0; i < dataCompacter.Length; i++)
-        {
-            // Console.WriteLine(result);
-            result += i * (dataCompacter[i]-48);
-        }
+        // for(int i = 0; i < dataCompacter.Length; i++)
+        // {
+        //     // Console.WriteLine(result);
+        //     result += i * (dataCompacter[i]-48);
+        // }
 
         return result;
     }
--- obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef701b9e4a417fd482c39180fe6b90972308e9c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168b1e0d9cf1c91b579505026a40680157ea5f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
